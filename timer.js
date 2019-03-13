75581070aba1b79974dc72d0f0e3952c1cf6fb90
Dan Martin <danmartindev@gmail.com>
2019-03-12 23:40:49 -0400

    updated to better functionality

--- timer.js
+++ timer.js
@@ -1,22 +1,44 @@
+//add timer div to DOM
+$( "body").prepend(
+    `
+        <div id='socstop-div'>
+            hello there
+        </div>
+    `
+);
 
+//is the timer paused/is the user on a different tab
+var isPaused = false;
+//time remaining in timer
+var timeLeft;
 
+//set the moment to the time when the timer is set + the time the user chooses
 
-// $( "#debutton" ).click(function(){ 
-//     port.postMessage({hours: "0", minutes: "0", seconds: "5"});
-//   });
+$( document ).ready(function() {
+    looper(0, 0, 5);
+});
 
-$( "body").prepend("<div id='test-timer'>hello there</div>");
+//the timer loop
+function looper(h, mm, ss){
+    //setup the time to countdown to
+    var then = moment().add({hours: h, minutes: mm, seconds: ss});
+    
+    //the loop to update the timer
+    (function timerLoop() {
+        $(".difference").text(moment().to(then));
+        $("#socstop-div").text(countdown(then).toString());
+        if(countdown(then) > 0){
+            requestAnimationFrame(timerLoop);
+        } else {
+            $( "#socstop-div").append("<button id='socstop-reset'>Reset</button>");
+            alert("GET OFF TWITTER");
+        }
+    })();
+};
 
-var now = moment(); // new Date().getTime();  
-var then = moment().add(5, 'seconds'); // new Date(now + 60 * 1000);
+//selector for restart button
+$( "#socstop-div" ).on("click", "#socstop-reset",function(){ 
+    //alert("whatever");
+    looper(0, 0, 5);
+});
 
-
-(function timerLoop() {
-  $(".difference").text(moment().to(then));
-  $("#test-timer").text(countdown(then).toString());
-  if(countdown(then).toString() != 0){
-    requestAnimationFrame(timerLoop);
-  } else {
-    alert("ZERO");
-  }
-})();
