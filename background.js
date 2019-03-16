4ba1cc288cd6073e081d0431bb36f4c61cbd6371
Dan Martin <danmartindev@gmail.com>
2019-03-16 12:49:45 -0400

    Addded pause and resume functionality

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
 //dictionary for storing urls with tab id
 var urls = { };
 
-//list of all alarms
+//dictionary of all alarms
 var bgAlarms = [];
+//dictionary of paused alarms
+var paused = { };
 
 chrome.runtime.onInstalled.addListener(function() {
     //placeholder for query info to get tabs
     var queryInfo = { };
+    //clear all previous built up alarms
+    chrome.alarms.clearAll();
 
     //grab tabs already opened
     chrome.tabs.query(queryInfo, function (tabs) {
@@ -34,11 +38,6 @@ chrome.runtime.onInstalled.addListener(function() {
     });
 });
 
-//function for alarm trigger
-chrome.alarms.onAlarm.addListener(function( alarm ){
-    console.log("Alarm: " + alarm.name);
-    alert("Times Up!");
-});
 
 //connecting port for communication with popup
 chrome.runtime.onConnect.addListener(function(port) {
@@ -52,9 +51,30 @@ chrome.runtime.onConnect.addListener(function(port) {
             case "delete":
                 console.log("delete");
                 break;
+            case "pause":
+                console.log("pausing: " + JSON.stringify(msg.name));
+                pauseAlarm(msg.name, msg.addTime);
+                break;
+            case "resume":
+                //console.log("resuming: " + msg.name);
+                resumeAlarm(msg.name);
+                break;
         }
     });
 });
+
+//function for alarm trigger
+chrome.alarms.onAlarm.addListener(function( alarm ){
+    console.log("Alarm: " + alarm.name);
+    alert("Times Up!");
+    deleteAlarm(alarm);
+});
+
+function resumeAlarm(name){
+    var seconds = paused[name] / 1000; //get value of time left and convert to seconds
+    delete paused[name];
+    createAlarm(0, 0, seconds, name); //recreate alarm with 
+};
 
 function createAlarm(h, mm, ss, id){
     var date = new Date(); //create date object 
@@ -67,6 +87,32 @@ function createAlarm(h, mm, ss, id){
     chrome.alarms.create(id, {when: date.getTime()});
     //get the alarm and add it to the array
     chrome.alarms.get(id, function(alarm){
-        bgAlarms.push(alarm);
+        bgAlarms[id] = alarm;
     });
 }
+
+function deleteAlarm(al){
+    var index = bgAlarms.indexOf(al);
+    bgAlarms.splice(index, 1);     //remove from bg array
+    chrome.alarms.clear(al.name); //remove from chrome alarms
+}
+
+/*
+*   pauses alarm by deleting the alarm and replacing
+*   must be done as changing the scheduled time does
+*   not stop the alarm from firing at the original declared time  
+*/  
+function pauseAlarm(name, time){
+    chrome.alarms.get(name, function(alarm){
+        deleteAlarm(alarm);
+    });
+    //paused.push({name: name, time: time}); //save time remaining to later reinstate alarm
+    paused[name] = time;
+    console.log("paused: " + JSON.stringify(paused));
+}
+
+ // chrome.alarms.getAll(function(alarms){
+    //     alarms.forEach(alarm => {
+    //         console.log("alarms: " + JSON.stringify(alarm));
+    //     });
+    // });
