379641e7ead8e74ade05ae02e46f6332a5fb41a3
Dan Martin <danmartindev@gmail.com>
2019-03-20 21:58:30 -0400

    restructured to now add default alarms

--- options.js
+++ options.js
@@ -1,15 +1,63 @@
-let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      page.appendChild(button);
+var urls;
+
+$( document ).ready(function() {
+  //get the local saved version of the urls array
+  //var call = "call";
+  //var two = "calling";
+  // var match = call.test(two);
+  // if(two.match(call)){
+  //   console.log("matched");
+  // } else {
+  //   console.log("no match");
+  // };
+  //console.log(match);
+  chrome.storage.sync.get(['urls'], function(result){
+    console.log(result.urls);
+    if(!result.urls){
+      //if null return an empty dictionary
+      console.log("null");
+      urls={};
+    } else {
+      urls = result.urls;
+      console.log("Get urls 1: " + JSON.stringify(urls));
     }
-  }
-  constructOptions(kButtonColors);
+    init();
+  });
+
+  $("#lBtn").click(function(){
+    chrome.storage.sync.get(['urls'], function(result){
+      console.log("Get res: " + JSON.stringify(result));
+      console.log("Get res: " + JSON.stringify(result.urls));
+    });
+    chrome.storage.sync.get(['urls'], function(result){
+      urls = {};
+   });
+  });
+
+  $("#clearBtn").click(function(){
+    chrome.storage.sync.clear(function(){
+      var error = chrome.runtime.lastError;
+      if (error) {
+        console.error(error);
+      }
+    });
+  });
+
+  $( "form" ).submit(function(event){
+    var fields = $( this ).serializeArray(); //serialize values into an array
+    urls[fields[0].value] = [fields[1].value, fields[2].value, fields[3].value];
+
+    chrome.storage.sync.set({'urls': urls}, function(){
+      console.log("Set urls");
+    });
+    $( this ).trigger("reset"); //reset the form values
+  });
+});
+
+function init(){
+  console.log("key");
+
+  for(var key in urls){
+    $('#set-urls').append("<li> URL: " + key + " H: " + urls[key][0]+ " M: " + urls[key][1]+ " S: " + urls[key][2]+ "</li>"); //updating paused alarm info
+  };
+}
