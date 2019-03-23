eabe987367da830ff08360e46df7621717fa1736
Dan Martin <danmartindev@gmail.com>
2019-03-22 20:34:48 -0400

    - added delete button to default alrams - added value checking to inputs

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 /* options.js created for Social Media Stopwatch
-* Updated 3/21/2019
+* Updated 3/22/2019
 * Author: Daniel Martin
 * website: www.dmartin.me
 * Github: github.com/danmartindev
@@ -41,13 +41,38 @@ $( document ).ready(function() {
     });
   });
 
+  $("#set-urls").on("click", ".delete-btn", function(){
+    var removeId = $(this).parent().attr('id');
+    delete urls[removeId];
+    $(this).parent().remove();
+    chrome.storage.sync.set({'urls': urls}, function(){
+      console.log("deleted url");
+    });
+
+  });
+
   $( "form" ).submit(function(event){
     var fields = $( this ).serializeArray(); //serialize values into an array
-    urls[fields[0].value] = [fields[1].value, fields[2].value, fields[3].value];
+    var hasNan = false;
+    for(var i = 1; i < fields.length; i++){
+      //sets value to 0 for emty input
+      if(fields[i].value == ""){
+        fields[i].value = 0;
+      }
+      //checks values for NaN
+      else if(isNaN(parseInt(fields[i].value))){
+        hasNan = true;
+      }
+    }
 
-    chrome.storage.sync.set({'urls': urls}, function(){
-      console.log("Set urls");
-    });
+    if(!hasNan){
+      urls[fields[0].value] = [fields[1].value, fields[2].value, fields[3].value];
+      chrome.storage.sync.set({'urls': urls}, function(){
+        console.log("Set urls");
+      });
+    } else {
+      alert("Please enter a valid number value to create an alarm");
+    }
     $( this ).trigger("reset"); //reset the form values
   });
 });
@@ -56,6 +81,6 @@ function init(){
   console.log("key");
 
   for(var key in urls){
-    $('#set-urls').append("<li> URL: " + key + " H: " + urls[key][0]+ " M: " + urls[key][1]+ " S: " + urls[key][2]+ "</li>"); //updating paused alarm info
+    $('#set-urls').append("<li id='" + key +"'><i class='delete-btn fas fa-times fa'></i> URL: " + key + " H: " + urls[key][0]+ " M: " + urls[key][1]+ " S: " + urls[key][2]+ "</li>"); //updating paused alarm info
   };
 }
